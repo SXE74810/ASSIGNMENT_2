b2c0f598e3ffde07bf7200e811eb446b69d3c1ba
700747481_SAI_AKHIL_EDA <122653844+SXE74810@users.noreply.github.com>
2023-02-03 16:54:56 -0600

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,8 @@
 my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
+y=[]
 #2 refers the step size in the range function
 for i in range(0,len(my_list),2):
-    print(my_list[i])
+    y.append(my_list[i])
+
+print(y)
